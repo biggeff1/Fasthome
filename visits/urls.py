c73d828ad9f66ddc219b1f84402ea53e9ab54ca0
biggeff1 <ntambwej505@gmail.com>
2026-08-20 16:42:55 +0200

    Add visit decision routes

--- visits/urls.py
+++ visits/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('request/<str:property_id>/', views.request_visit, name='request_visit'),
+    path('<str:visit_id>/landlord-decision/', views.landlord_visit_decision, name='landlord_visit_decision'),
+    path('<str:visit_id>/decision/', views.tenant_decision, name='tenant_decision'),
 ]
