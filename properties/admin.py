a2306b10600705da4567baf49fe90edd01fcce1f
biggeff1 <ntambwej505@gmail.com>
2026-08-20 16:45:11 +0200

    Register property models in admin

---
+++ properties/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import (Bathroom, Bedroom, CollaborationConsent, Favorite, Kitchen, LivingRoom, Property, PropertyDeclaration, PropertyFeature, PropertyPhoto, PropertyPublication, PropertyType, Toilet)
+
+admin.site.register(PropertyType)
+admin.site.register(Property)
+admin.site.register(PropertyFeature)
+admin.site.register(Bedroom)
+admin.site.register(LivingRoom)
+admin.site.register(Kitchen)
+admin.site.register(Bathroom)
+admin.site.register(Toilet)
+admin.site.register(PropertyPhoto)
+admin.site.register(PropertyPublication)
+admin.site.register(PropertyDeclaration)
+admin.site.register(CollaborationConsent)
+admin.site.register(Favorite)
