686619d0ddd669f726dcdc8e37f5bbefd7fb2c7e
biggeff1 <ntambwej505@gmail.com>
2026-08-20 16:38:29 +0200

    Create Django project configuration

---
+++ config/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('properties.urls')),
+    path('accounts/', include('users.urls')),
+    path('matching/', include('matching.urls')),
+    path('visits/', include('visits.urls')),
+    path('leasing/', include('leasing.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
