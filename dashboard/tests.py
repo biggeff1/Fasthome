86da80393083098ee88375cd853f48adafc9f23a
biggeff1 <ntambwej505@gmail.com>
2026-08-21 10:12:50 +0200

    Include favorites workflow tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -2,3 +2,4 @@ from .access_tests import *  # noqa: F401,F403
 from .publication_tests import *  # noqa: F401,F403
 from .verification_tests import *  # noqa: F401,F403
 from .property_flow_tests import *  # noqa: F401,F403
+from .favorites_tests import *  # noqa: F401,F403
