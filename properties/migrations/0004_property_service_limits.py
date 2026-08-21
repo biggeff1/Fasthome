e3810d864adde44ada431723c59014d936ddb873
biggeff1 <ntambwej505@gmail.com>
2026-08-21 22:43:48 +0200

    Ajouter les contraintes DB des disponibilités

---
+++ properties/migrations/0004_property_service_limits.py
@@ -0,0 +1,26 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('properties', '0003_property_services'),
+    ]
+
+    operations = [
+        migrations.AddConstraint(
+            model_name='property',
+            constraint=models.CheckConstraint(
+                condition=models.Q(electricity_days_per_week__isnull=True)
+                | models.Q(electricity_days_per_week__lte=7),
+                name='property_electricity_days_max_7',
+            ),
+        ),
+        migrations.AddConstraint(
+            model_name='property',
+            constraint=models.CheckConstraint(
+                condition=models.Q(water_days_per_week__isnull=True)
+                | models.Q(water_days_per_week__lte=7),
+                name='property_water_days_max_7',
+            ),
+        ),
+    ]
