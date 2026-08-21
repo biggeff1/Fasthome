1760bb3ebfee6ce8a3d5448512c25f74cffbac1f
biggeff1 <ntambwej505@gmail.com>
2026-08-21 10:12:22 +0200

    Include property workflow tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,4 @@
 from .access_tests import *  # noqa: F401,F403
 from .publication_tests import *  # noqa: F401,F403
 from .verification_tests import *  # noqa: F401,F403
+from .property_flow_tests import *  # noqa: F401,F403
