b2d101598f4de7a35f8f41eb8ff1660d3bb646b0
biggeff1 <ntambwej505@gmail.com>
2026-08-21 15:14:59 +0200

    Inclure les tests des espaces Agent/Admin

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -4,3 +4,4 @@ from .verification_tests import *  # noqa: F401,F403
 from .property_flow_tests import *  # noqa: F401,F403
 from .favorites_tests import *  # noqa: F401,F403
 from .final_hardening_tests import *  # noqa: F401,F403
+from .office_space_tests import *  # noqa: F401,F403
