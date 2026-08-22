0e2f73ffc87a2f7d9bae733429adc8c7fe22548c
biggeff1 <ntambwej505@gmail.com>
2026-08-22 05:20:19 +0200

    Chargement du référentiel de localisation au démarrage

--- properties/apps.py
+++ properties/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
+
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        from . import location_models  # noqa: F401
