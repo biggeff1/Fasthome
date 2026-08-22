645d77c46a34ac1263c6e7b5c9c96e4a223f7ef9
biggeff1 <ntambwej505@gmail.com>
2026-08-22 17:58:22 +0200

    Activate optimized publication photo policy

--- properties/apps.py
+++ properties/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        from .photo_optimization import install
+        install()
