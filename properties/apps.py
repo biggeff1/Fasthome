7cac2b2cb0e557e74283a2e7faa71379142df8b5
biggeff1 <ntambwej505@gmail.com>
2026-08-22 17:24:17 +0200

    Supprimer les hooks du système parent-enfant de localisation

--- properties/apps.py
+++ properties/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
-
-    def ready(self):
-        from . import location_models  # noqa: F401
-        from . import location_signals  # noqa: F401
