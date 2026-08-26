03a7849f894ea821198598d06c9d4adb649087df
biggeff1 <ntambwej505@gmail.com>
2026-08-26 10:41:39 +0200

    admin: appliquer les libellés français au démarrage

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -5,3 +5,9 @@ class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
     verbose_name = 'Administration Fasthome'
+
+    def ready(self):
+        # django.contrib.admin est placé avant dashboard dans INSTALLED_APPS,
+        # donc son autodiscover a déjà enregistré les ModelAdmin à ce stade.
+        from .admin_labels import apply_french_admin_labels
+        apply_french_admin_labels()
