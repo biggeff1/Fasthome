14db74cb90335b538cc53d33fb920f523798ed06
biggeff1 <ntambwej505@gmail.com>
2026-08-26 17:08:36 +0200

    Ajouter la route de suppression des brouillons

--- dashboard/urls_property_management.py
+++ dashboard/urls_property_management.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .my_properties_views import my_properties, property_manage
+from .draft_delete import property_delete_draft
 
 urlpatterns = [
     path('properties/', my_properties, name='my_properties'),
     path('properties/<str:property_id>/manage/', property_manage, name='property_manage'),
+    path('properties/<str:property_id>/delete-draft/', property_delete_draft, name='property_delete_draft'),
 ]
