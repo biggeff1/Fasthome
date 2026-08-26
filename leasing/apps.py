4bc85fe1c925975171721d7660125e2bfd018359
biggeff1 <ntambwej505@gmail.com>
2026-08-26 10:40:24 +0200

    admin: traduire la rubrique locations

--- leasing/apps.py
+++ leasing/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
+
 class LeasingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'leasing'
+    verbose_name = 'Locations'
