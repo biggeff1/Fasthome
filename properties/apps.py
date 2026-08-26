59a82a98f641499a862e329978e2eb41c7e4d277
biggeff1 <ntambwej505@gmail.com>
2026-08-26 10:40:15 +0200

    admin: traduire la rubrique logements

--- properties/apps.py
+++ properties/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+    verbose_name = 'Logements'
 
     def ready(self):
         from .photo_optimization import install
